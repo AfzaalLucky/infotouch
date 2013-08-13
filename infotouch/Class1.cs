bd14d6cd383ce6c332885cdc7a92ca5edf85ee90
aybeeyutipul <ivyvanbravo@gmail.com>
2013-08-12 19:16:04 -0700

    objects done

--- infotouch/Class1.cs
+++ infotouch/Class1.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace infotouch
 {
-    class Class1
+    class Schedule
     {
+        public int ScheduleId { get;set; }
+        public string ScheduleCode { get; set; }
+        public string AmIn { get; set; }
+        public string AmOut { get; set; }
+        public string PmIn { get; set; }
+        public string PmOut { get; set; }
+    }
+
+    class Attendance
+    {
+        public int AttendanceId { get; set; }
+        public DateTime TrDate { get; set; }
+        public int EmpNum { get; set; }
+        public DateTime TrAmIn { get; set; }
+        public DateTime TrAmOut { get; set; }
+        public DateTime TrPmIn { get; set; }
+        public DateTime TrPmOut { get; set; }
     }
 }
